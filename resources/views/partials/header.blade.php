df1a44030875903ea619ca18c959aad75564a6c1
Bilobra <bra.carrozzo@gmail.com>
2022-10-16 18:40:37 +0200

    add files img + style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,24 @@
 <header>
-    <nav>
-        <ul>
-            <li>
-                <a href="{{route('homepage')}}">
-                    Home
-                </a>
-            </li>
-            <li>
-                <a href="{{route('comics.index')}}">
-                    Comics
-                </a>
-            </li>
-        </ul>
-    </nav>
+    <div class="container">
+        <div class="nav-wrapper">
+            <div class="logo">
+                <img src="{{asset('img/dc-logo.png')}}" alt="logo dc">
+            </div>
+            <nav class="nav">
+                <ul >
+                    <li class="nav-link">
+                        <a href="{{route('homepage')}}">
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="{{route('comics.index')}}">
+                            Comics
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+    </div>
 </header>
